5b9e78702f2cdd82853743f103998cebde881b8d
Yuan Liang <yuan@yuans-MacBook-Pro.local>
2016-01-19 16:07:32 -0500

    second commit

---
+++ opengl/opengl/hw5.h
@@ -0,0 +1,13 @@
+//
+//  hw5.h
+//  opengl
+//
+//  Created by yuan on 11/29/15.
+//  Copyright (c) 2015 yuan. All rights reserved.
+//
+
+#ifndef opengl_hw5_h
+#define opengl_hw5_h
+
+
+#endif
---
+++ opengl/opengl/main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  opengl
+//
+//  Created by yuan on 11/29/15.
+//  Copyright (c) 2015 yuan. All rights reserved.
+//
+
+#include "main.h"
